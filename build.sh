67c5d0a3e0205e2d3f3de8ed855ecdbf6c1372e5
Evan Gamble <egamble@factual.com>
2014-07-31 13:02:40 -0700

    LEIN_ROOT=true

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 ENDPOINT=${1:-"ladder"}
 WEBAPP_TARGET=$CATALINA_BASE/webapps/$ENDPOINT
 sudo mkdir -p $WEBAPP_TARGET
-lein ring uberwar
+LEIN_ROOT=true lein ring uberwar
 cp target/elo-sport-1.0.0-SNAPSHOT-standalone.war $WEBAPP_TARGET/root.war
 cd $WEBAPP_TARGET
 jar -xf root.war
