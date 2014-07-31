4759290581c762a077d272d0d9cfa5dea58da3f1
Evan Gamble <egamble@factual.com>
2014-07-31 14:49:58 -0700

    strip old chatbot stuff

--- src/elo_sport/core.clj
+++ src/elo_sport/core.clj
@@ -1,33 +1,6 @@
 (ns elo-sport.core
-  (:require [elo-sport.register :refer [responders]]
-            [elo-sport.responder.nonsense]
-            [elo-sport.responder.sleeper]
-            [elo-sport.responder.bored]
-            [elo-sport.responder.reflector]
-            [elo-sport.responder.delegator]
-            [elo-sport.history    :refer [add-to-history!]]
-            [elo-sport.middleware :refer [tokenize is-question?]]
-            [clojure.string :as str]))
+  (:require [clojure.string :as str]))
 
-(defn analyzers [m]
-  (-> m
-      tokenize
-      is-question?))
-
-(def timeout-period 200)
-
-(defn query [input & [chat-session-id]]
-  (let [input-map (analyzers {:input input :chat-session-id chat-session-id})
-        sorted-responders (sort-by (comp - :confidence)
-                                   (map #(% input-map)
-                                        (vals @responders)))
-        output-map (some #(deref (:response-ref %)
-                                 timeout-period
-                                 nil)
-                       sorted-responders)]
-    (when chat-session-id
-      (add-to-history! chat-session-id [input-map output-map]))
-    (:output output-map)))
 
 (defn new-score [current-score game-score expected-score]
   (let [k 32]
@@ -63,11 +36,3 @@
   (let [valid-matches (filter #(< (:timestamp %) time) matches)
         sorted-matches (sort-by :timestamp valid-matches)]
     (reduce update-elo-scores scores sorted-matches)))
-
-(defn chat-loop []
-  (loop []
-    (print "Eliza> ")
-    (flush)
-    (when-let [input (not-empty (read-line))]
-      (println (query (str/trim input)))
-      (recur))))
