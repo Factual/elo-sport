904fba9489a9ba26679e4ca59eff0c93742b41b6
Evan Gamble <egamble@factual.com>
2014-07-31 12:31:46 -0700

    unjar into webapps

--- build.sh
+++ build.sh
@@ -2,4 +2,7 @@ ENDPOINT=${1:-"ladder"}
 WEBAPP_TARGET=$CATALINA_BASE/webapps/$ENDPOINT
 sudo mkdir -p $WEBAPP_TARGET
 lein ring uberwar
-sudo cp target/elo-sport-1.0.0-SNAPSHOT-standalone.war $WEBAPP_TARGET/root.war
+cp target/elo-sport-1.0.0-SNAPSHOT-standalone.war $WEBAPP_TARGET/root.war
+cd $WEBAPP_TARGET
+jar -xf root.war
+rm root.war
