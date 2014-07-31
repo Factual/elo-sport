a46423a5b507bae06fe51d680b654deb89bebf6c
Evan Gamble <egamble@factual.com>
2014-07-31 11:58:10 -0700

    build script

---
+++ build.sh
@@ -0,0 +1,5 @@
+ENDPOINT=${1:-"ladder"}
+WEBAPP_TARGET=$CATALINA_BASE/webapps/$ENDPOINT
+sudo mkdir -p $WEBAPP_TARGET
+lein ring uber
+sudo cp target/elo-sport-1.0.0-SNAPSHOT-standalone.war $WEBAPP_TARGET/root.war
