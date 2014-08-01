59f9fe5e2e70d618553c58989e66b4c25bf75934
Stephanie Porter <stephanie@factual.com>
2014-08-01 14:09:54 -0700

    Update create challenge to send emails

--- src/elo_sport/challenge.clj
+++ src/elo_sport/challenge.clj
@@ -1,15 +1,19 @@
 (ns elo-sport.challenge
-  (:require [elo-sport.db :as db])
+  (:require [elo-sport.db :as db]
+            [elo-sport.email :as email])
   (:use monger.operators))
 
 (def FORFEIT-WIN-SCORE 3)
 (def FORFEIT-LOSS-SCORE 0)
 
 (defn create-challenge
-  [challenger opponent]
-  (let [matches (db/get-matches {:status :open $or [{:challenger challenger :opponent opponent} {:challenger opponent :opponent challenger}]})]
-    (if (empty? matches) (db/insert-match challenger opponent)
-        nil)))
+  [challenger opponent message]
+  (let [matches (db/get-matches {:status :open $or [{:challenger challenger :opponent opponent} {:challenger opponent :opponent challenger}]})
+        insert-match-return (if (empty? matches) (db/insert-match challenger opponent))
+        ch-map (db/get-players {:username challenger})
+        op-map (db/get-players {:username opponent})
+        match (db/get-matches {:status :open :challenger challenger :opponent opponent})]
+    (if (insert-match-return) (email/send-challenge-email ch-map op-map (first match) message))))
 
 (defn update-challenge
   [challenger opponent challenger-score opponent-score note]
