a06408ff51e11bc52e7ca2d617e006544f6a3b68
Evan Gamble <egamble@factual.com>
2014-08-01 11:25:44 -0700

    login page

--- src/elo_sport/server.clj
+++ src/elo_sport/server.clj
@@ -1,34 +1,47 @@
 (ns elo-sport.server
-  (:use  [ring.middleware params keyword-params resource session]
-         [elo-sport.views ladder])
   (:require [compojure.core  :refer :all]
             [compojure.route :as route]
-            [ring.util.response :as resp]
-            [elo-sport.db :as db]))
+            [elo-sport.db :as db]
+            [elo-sport.views
+             [ladder :refer [ladder-page]]]
+            [hiccup
+             [form :refer :all]
+             [core :refer :all]
+             [page :refer :all]
+             [element :refer :all]]))
 
 
-(defn exception-str
-  [e]
+(defn exception-str [e]
   (with-out-str (.printStackTrace e (java.io.PrintWriter. *out*))))
 
 
-(defn login-handler
-  [{:keys [params] :as req}]
+(defn login-page [req] 
+  (html5
+   (form-to
+    [:post "authenticate"]
+    "Player name: "
+    [:input {:type "text"
+             :name "username"}]
+    (submit-button "Log in"))))
+
+
+(defn authenticate-handler [{:keys [params] :as req}]
   (let [username (:username params)
         session (:session req)]
     (if (and session (= username (:username session)))
-      "Already logged in."
-      {:body "Logged in."
+      ;; already logged in
+      (ladder-page req)
+      {:body (ladder-page req)
        :session {:username username
                  :session-id (.toString (java.util.UUID/randomUUID))}})))
 
 
 (defn logout-handler
-  [{:keys [params] :as req}]
+  [req]
   (if (get-in req [:session :username])
-    {:body "Logged out."
+    {:body (ladder-page req)
      :session {:username nil :session-id nil}}
-    "Not logged in."))
+    (ladder-page req)))
 
 
 (defn challenge-handler
@@ -48,8 +61,9 @@
 
 (defroutes elo-handlers
   (GET "/" [] ladder-page)
-  (POST "/login" [] login-handler)
-  (POST "/logout" [] logout-handler)
+  (GET "/login" [] login-page)
+  (GET "/logout" [] logout-handler)
+  (POST "/authenticate" [] authenticate-handler)
   (GET "/challenge" [] challenge-handler)
   (GET "/update" [] update-challenge)
   (route/not-found "Page not found."))
@@ -57,7 +71,6 @@
 
 (def app
   (-> elo-handlers
-      (wrap-resource "public")
       wrap-session
       wrap-keyword-params
       wrap-params))
