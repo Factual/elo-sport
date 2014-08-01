379afd3647731e3b4bf4a4a86fbee8421e03a10c
Evan Gamble <egamble@factual.com>
2014-07-31 19:28:45 -0700

    player table

--- src/elo_sport/views/ladder.clj
+++ src/elo_sport/views/ladder.clj
@@ -1,5 +1,6 @@
 (ns elo-sport.views.ladder
-  (:use [hiccup form core page element]))
+  (:use [hiccup form core page element])
+  (:require [elo-sport.rating :as rating]))
 
 
 (defn ladder-page [{:keys [params] :as req}]
@@ -12,4 +13,12 @@
       (if username
         [:div "Player: " username
          (link-to "logout" "Log out")]
-        (link-to "login" "Log in")))]))
+        (link-to "login" "Log in")))
+    (let [ratings (sort #(< (second %1) (second %2))
+                        (seq (rating/all-elo-ratings)))]
+      [:table
+       (map (fn [[player rating]]
+              [:tr
+               [:td player]
+               [:td rating]])
+            ratings)])]))
