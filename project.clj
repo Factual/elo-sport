5ae91af5c41f4b056c2ddce1a67cf3a51fa37741
Stephanie Porter <stephanie@factual.com>
2014-07-31 18:26:36 -0700

    Use db to calculate elo ratings; rename file; fix project.clj

--- project.clj
+++ project.clj
@@ -1,12 +1,10 @@
 (defproject elo-sport "1.0.0-SNAPSHOT"
   :description "Elo rankings for ping pong, pool and other sports."
   :jar-exclusions [#"\.git"] ; not necessary in a future release of leiningen
-  :main elo-sport.core
   :plugins [[lein-ring "0.8.10"]]
   :ring {:handler elo-sport.server/app}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [compojure "1.1.8"]
                  [com.novemberain/monger "2.0.0"]
-                 [org.clojure/core.async "0.1.303.0-886421-alpha"]
                  [hiccup "1.0.5"]])
 
