9e535a45d112051f8726ff3739c63bd791a310cd
Evan Gamble <egamble@factual.com>
2014-07-31 17:34:24 -0700

    simple input

--- src/elo_sport/views/ladder.clj
+++ src/elo_sport/views/ladder.clj
@@ -3,8 +3,12 @@
 
 (defn ladder-page []
   (html5
-    [:head
-      [:title "Hello World"]
-      (include-css "/css/style.css")]
-    [:body
-      [:h1 "Hello World"]]))
+   [:head
+    [:title "Factual Ping Pong Ladder"]]
+   [:body
+    [:h1 "Hello World"]
+    (form-to [:post "/login"]
+             "username:"
+             [:input {:type "text"
+                      :name "username"}]
+             (submit-button "Submit"))]))
